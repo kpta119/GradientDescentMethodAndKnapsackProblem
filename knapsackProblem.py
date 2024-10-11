fcf2ecd806273a478a9d995edb91f6116b38e027
Kacper Ptaszek <kacper.pt121@gmail.com>
2024-10-11 11:15:13 +0200

    Utworzenie pliku do zadania z problemem plecakowym

---
+++ knapsackProblem.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+m = np.array([8, 3, 5, 2]) #masa przedmiotów
+M = np.sum(m)/2 #niech maksymalna masa plecaka będzie równa połowie masy przedmiotów
+p = np.array([16, 8, 9, 6]) #wartość przedmiotów
