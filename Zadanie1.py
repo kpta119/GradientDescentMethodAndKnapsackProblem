c25309ab7c03ea7733cf2893909f9fce9fe73151
kpta119 <147331995+kpta119@users.noreply.github.com>
2024-10-26 13:09:05 +0200

    dodanie plików projektowych

---
+++ Zadanie1.py
@@ -0,0 +1,82 @@
+import numpy as np
+import random
+import time
+
+
+
+def knapsackProblemBruteForce(m, p, Wmax):
+    n = len(m)
+    maxValue = 0
+    for i in range(2**n):
+        currentWeight = 0
+        currentValue = 0
+        for j in range(n):
+            if (i & (2**(n-1-j))):
+                if currentWeight + m[j] <= Wmax:
+                    currentWeight += m[j]
+                    currentValue += p[j]
+
+        maxValue = max(currentValue, maxValue)
+    return maxValue
+
+
+def sortElements(m, p):
+    n = len(m)
+    for i in range(n):
+        for j in range(n-1):
+            if p[j]/m[j] < p[j+1]/m[j+1]:
+                tempValue = p[j+1]
+                tempWeight = m[j+1]
+                p[j+1] = p[j]
+                m[j+1] = m[j]
+                p[j] = tempValue
+                m[j] = tempWeight
+    return (m,p)
+
+
+def packElemetsUsingPMRatio(m,p, Wmax):
+    m, p = sortElements(m, p)
+    n = len(m)
+    currentWeight = 0
+    maxValue = 0
+    for i in range(n):
+        if currentWeight + m[i] <= Wmax:
+            currentWeight += m[i]
+            maxValue += p[i]
+        
+    return maxValue
+
+
+
+def generateKnapsackProblems(amount):
+    weightRow = np.array([], dtype=int)
+    valueRow = np.array([], dtype=int)
+    for i in range(amount):
+        randomWeight = random.randint(1,10)
+        weightRow = np.append(weightRow, [randomWeight])
+        randomValue = random.randint(1,20)
+        valueRow = np.append(valueRow, [randomValue])
+    return weightRow, valueRow
+
+
+
+def main():
+    m = np.array([8, 3, 5, 2]) #masa przedmiotów
+    M = np.sum(m)/2 #niech maksymalna masa plecaka będzie równa połowie masy przedmiotów
+    p = np.array([16, 8, 9, 6]) #wartość przedmiotów
+    amount = 16
+    weights, values = generateKnapsackProblems(amount)
+    Wmax = int(np.sum(weights)/2)
+    # start = time.process_time()
+    packElemetsUsingPMRatio(weights,values,Wmax)
+    # end = time.process_time()
+    # total = end - start
+    # print(f"{total:.8f}s \n")
+    # start = time.process_time()
+    knapsackProblemBruteForce(weights,values,Wmax)
+    # end = time.process_time()
+    # total = end - start
+    # print(f"{total:.8f}s \n")
+
+if __name__ == "__main__":
+    main()
---
+++ Zadanie2.py
@@ -0,0 +1,64 @@
+from autograd import grad
+import autograd.numpy as np
+from cec2017.functions import f1, f2, f3
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+def booth(x):
+    x1 = x[0]
+    x2 = x[1]
+    return (x1+2*x2-7)**2+(2*x1+x2-5)**2
+
+
+def gradientDescentMethod(function, maxIterations, dimensionality=10, beta=0.01):
+    UPPER_BOUND = 100
+    x = np.random.uniform(-UPPER_BOUND, UPPER_BOUND, size=dimensionality)
+    grad_fct = grad(function)
+    gradient = grad_fct(x)
+    iteration = 0
+    while np.linalg.norm(gradient) > 1e-5:
+        xPrevious = x
+        gradient = grad_fct(x)
+        x = x - beta*gradient
+        x = np.clip(x, -UPPER_BOUND, UPPER_BOUND)
+        changeOnAxisX = x[0] - xPrevious[0]
+        changeOnAxisY = x[1] - xPrevious[1]
+        plt.arrow(xPrevious[0],xPrevious[1],changeOnAxisX, changeOnAxisY, head_width=2, head_length=1, fc='k', ec='k')
+        iteration += 1
+        if iteration > maxIterations:
+            break
+    return x
+
+def drawPlot(function):
+    MAX_X = 100
+    PLOT_STEP = 0.1
+
+    x_arr = np.arange(-MAX_X, MAX_X, PLOT_STEP)
+    y_arr = np.arange(-MAX_X, MAX_X, PLOT_STEP)
+    X, Y = np.meshgrid(x_arr, y_arr)
+    Z = np.empty(X.shape)
+
+    q=function
+    for i in range(X.shape[0]):
+        for j in range(X.shape[1]):
+            Z[i, j] = q(np.array([X[i, j], Y[i, j]]))
+
+    contour = plt.contour(X, Y, Z, 30)
+    fmt = ticker.FormatStrFormatter("%.1e")
+    plt.clabel(contour, inline=True, fontsize=6, fmt=fmt)
+
+    plt.title(f"Function: {function.__name__}" )
+    plt.xlabel("x1")
+    plt.ylabel("x2")
+
+
+
+
+def main():
+    function = booth
+    drawPlot(function)
+    x = gradientDescentMethod(function, 10000, 2, 0.01)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
